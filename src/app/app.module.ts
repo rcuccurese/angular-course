1cf00a9900c8bc38d886b74d0d397f4e354ffd1a
rcuccurese <roberto.cuccurese@alten.it>
2020-07-07 00:08:56 +0200

    4 files modified, 10 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,6 @@ import { MDBBootstrapModule } from 'angular-bootstrap-md';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
-import { SharedModule } from './shared/shared.module';
 import { CardComponent } from './components/card/card.component';
 import { NavBarComponent } from './core/nav-bar/nav-bar.component';
 import { RatingsComponent } from './containers/ratings/ratings.component';
@@ -37,8 +36,7 @@ import { CardListComponent } from './components/card-list/card-list.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    MDBBootstrapModule.forRoot(),
-    SharedModule
+    MDBBootstrapModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
